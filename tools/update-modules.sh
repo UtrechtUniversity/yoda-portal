77e5d357d741a7f2fc0fb31cbda14b7885f21b3b
Chris Smeele <cj.smeele@xs4all.nl>
2015-07-23 14:20:19 +0200

    Added update-portal.sh and improved update-module.sh

--- tools/update-modules.sh
+++ tools/update-modules.sh
@@ -6,9 +6,12 @@ cd modules
 
 for module in *; do
 	if [[ -d $module ]]; then
-		echo "Updating module $module"
 		cd $module
+		echo "--------"
+		echo -e "Updating module \e[33;1m$module\e[0m on branch \e[32;1m`git rev-parse --abbrev-ref HEAD`\e[0m"
+		echo "- Old version: `git show --no-patch --format='%h - %s'`"
 		git pull
+		echo "- New version: `git show --no-patch --format='%h - %s'`"
 		cd ..
 	fi
 done
---
+++ tools/update-portal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo -e "Updating \e[33;1myoda-portal\e[0m on branch \e[32;1m`git rev-parse --abbrev-ref HEAD`\e[0m"
+echo "- Old version: `git show --no-patch --format='%h - %s'`"
+git pull
+echo "- New version: `git show --no-patch --format='%h - %s'`"
+
+echo "Updating modules"
+
+tools/update-modules.sh
