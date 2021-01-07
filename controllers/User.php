c3b9f5bbaba8f0c74ddf1566397613fe08bde055
Rick van de Hoef <r.e.a.vandehoef@uu.nl>
2021-01-07 11:16:15 +0100

    First steps for oidc, not complete

--- controllers/User.php
+++ controllers/User.php
@@ -10,20 +10,25 @@ class User extends MY_Controller {
 
     public function login()
     {
+	// Get these from a config file
+	$clientId = "myClientID";
+	$redirectUri = "https://portal.yoda.test/user/callback";
+	$authUri = "https://oauth.mocklab.io/oauth/authorize?response_type=code&client_id={$clientId}&redirect_uri={$redirectUri}";
+
         // Redirect logged in users to home.
         if ($this->rodsuser->isLoggedIn()) {
             redirect('home');
         }
 
-        $this->session->unset_userdata('username');
-        $this->session->unset_userdata('password');
+        //$this->session->unset_userdata('username');
+        //$this->session->unset_userdata('password');
 
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
+        //$username = $this->input->post('username');
+        //$password = $this->input->post('password');
 
-        $loginFailed = false;
+        //$loginFailed = false;
 
-        if (isset($username) && isset($password) && $username !== false && $password !== false) {
+        /*if (isset($username) && isset($password) && $username !== false && $password !== false) {
             if ($this->rodsuser->login($username, $password)) {
                 $this->session->set_userdata('username', $username);
                 $this->session->set_userdata('password', $password);
@@ -48,7 +53,82 @@ class User extends MY_Controller {
             'loginFailed'  => $loginFailed,
         );
 
+        loadView('user/login', $viewParams);*/
+	//type redirect aanpassen (nu temporary)
+	redirect($authUri);
+    }
+
+    public function callback($code = '') {
+	$code = $this->input->get('code', TRUE);
+	
+	if($code == '') {
+		$code = "abc123";
+	}
+	
+	$tokenUrl = 'https://oauth.mocklab.io/oauth/token';
+	$callbackUri = 'https://portal.yoda.test/user/callback';
+	$clientId = 'myClientID';
+	$clientSecret = 'myClientPassword';
+	$grant_type = 'autorization_code';
+	$CREDS = base64_encode("$clientId:$clientSecret");
+
+	$formdata = array(
+		'grant_type' => 'authorization_code', 
+		'code' => $code, 
+		'redirect_uri' => $callbackUri
+	);
+
+	$options = [
+		'http' => [
+			  'header' => [ "Authorization: Basic $CREDS"
+			      	      , "Content-Type: application/x-www-form-urlencoded"
+				      ]
+			, 'method' => 'POST'
+			, 'content' => http_build_query($formdata)
+			]
+	];
+
+	$context = stream_context_create($options);
+	$result = file_get_contents($tokenUrl, false, $context);
+	$jsonresult = json_decode($result, TRUE);
+
+	$claimElement = explode('.', $jsonresult['id_token'])[1];
+	$claimData = json_decode(base64_decode($claimElement), TRUE);
+
+	$username = $claimData['email'];
+	$password = $jsonresult['access_token'];
+	
+//	$data = array( 'code' => $code, 'result' => $result, 'user' => $username, 'token' => $password );
+//	$this->load->view('user/callback', $data);
+
+	$loginFailed = false;
+
+        if ($this->rodsuser->login($username, $password)) {
+	    $this->session->set_userdata('username', $username);
+	    $this->session->set_userdata('password', $password);
+	    // TODO: Set iRODS temporary password instead.
+
+            $redirectTarget = $this->session->flashdata('redirect_after_login');
+
+                if ($redirectTarget === false)
+                    redirect('home');
+                else
+                    redirect($redirectTarget);
+        } 
+	else {
+            $loginFailed = true;
+        }
+
+        $this->session->keep_flashdata('redirect_after_login');
+
+        $viewParams = array(
+            'activeModule' => 'login',
+            'scriptIncludes' => array('js/login.js'),
+            'loginFailed'  => $loginFailed,
+        );
+
         loadView('user/login', $viewParams);
+
     }
 
     public function logout() {
---
+++ views/user/callback.php
@@ -0,0 +1,11 @@
+HOI
+<?php
+	echo "Chode: {$code}\n\n";
+	echo "Result:\n\n";
+
+	var_dump($result);
+
+	echo '<br/><br/>';
+	echo "User: $user, token: $token";
+?>
+<a href="https://portal.yoda.test">Terug naar af</a>
