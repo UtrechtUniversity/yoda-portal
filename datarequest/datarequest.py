c827c15bd33efd28e9c2732fc77fa5822c2d9247
Rick van de Hoef <r.e.a.vandehoef@uu.nl>
2021-03-23 13:41:19 +0100

    Linting for intake and datarequest, added to setup.cfg

--- datarequest/datarequest.py
+++ datarequest/datarequest.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+
+__copyright__ = 'Copyright (c) 2021, Utrecht University'
+__license__   = 'GPLv3, see LICENSE'
+
 from flask import Blueprint, render_template
 
-datarequest_bp = Blueprint('datarequest_bp', __name__,
-                     template_folder='templates/datarequest',
-                     static_folder='static/datarequest')
+datarequest_bp = Blueprint(
+    'datarequest_bp',
+    __name__,
+    template_folder='templates'
+)
 
 
 @datarequest_bp.route('/example')
--- intake/intake.py
+++ intake/intake.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+
+__copyright__ = 'Copyright (c) 2021, Utrecht University'
+__license__   = 'GPLv3, see LICENSE'
+
 from flask import Blueprint, render_template
 
-intake_bp = Blueprint('intake_bp', __name__,
-                     template_folder='templates/intake',
-                     static_folder='static/intake')
+intake_bp = Blueprint(
+    'intake_bp',
+    __name__,
+    template_folder='templates'
+)
 
 
 @intake_bp.route('/example')
