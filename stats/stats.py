757676d37be434fc7469afff8873986f810fb7cd
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-03-30 11:49:30 +0200

    Export CSV with statistics.

--- stats/stats.py
+++ stats/stats.py
@@ -3,6 +3,8 @@
 __copyright__ = 'Copyright (c) 2021, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
+from datetime import datetime
+
 from flask import Blueprint, make_response, render_template
 
 import api
@@ -24,7 +26,26 @@ def index():
 
 @stats_bp.route('/export')
 def export():
-    output = make_response("cvs, file, test\n1 , 2, 3")
+    response = api.call('resource_monthly_category_stats', data={})
+
+    csv = "category;subcategory;groupname;tier;"
+
+    months = ['January', 'February', 'March', 'April',
+              'May', 'June', 'July', 'August',
+              'September', 'October', 'November', 'December']
+    current_month = datetime.now().month
+    for i in range(11, -1, -1):
+        month = (current_month - i) + 12 if (current_month - i) < 0 else current_month - i
+        csv += "{};".format(months[month - 1])
+    csv += "\n"
+
+    for stat in response['data']:
+        csv += "{};{};{};{};".format(stat['category'], stat['subcategory'], stat['groupname'], stat['tier'])
+        for month in stat['storage']:
+            csv += "{};".format(month)
+        csv += "\n"
+
+    output = make_response(csv)
     output.headers["Content-Disposition"] = "attachment; filename=export.csv"
     output.headers["Content-type"] = "text/csv"
     return output
