dde75a1676386f047a14afb8c6638016f3e05876
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-04-06 08:27:09 +0200

    Add statistics module (#15)
    * Add researcher group statistics.
    
    * Fix url for stats static files.
    
    * Fix stats static files url.
    
    * Call new group list API.
    
    * Rewrite javascript to match new resource_full_year_group_data API.
    
    * Add missing chart library.
    
    * Fix path to Chartjs.
    
    * Fix storage chart.
    
    * Add category statistics.
    
    * Category storage in TB.
    
    * Fix header scripts block.
    
    * Create route for statistics export.
    
    * Fix url for statistics export.
    
    * Export CSV with statistics.
    
    * Fix missing stylesheet for statistics.
    
    * Fix stylesheet extension.

---
+++ stats/static/stats/css/stats.css
@@ -0,0 +1,54 @@
+.grey,
+i.form-control-feedback.glyphicon {
+    color: #aaa;
+}
+
+.select2-highlighted .grey {
+    color: #ddd;
+}
+
+.panel > .list-group .list-group .list-group-item {
+    border-bottom: 1px solid #ddd;
+}
+
+.panel > .list-group .list-group {
+    margin-top: 0;
+    margin-bottom: 0;
+}
+.panel > .list-group .list-group .list-group .list-group-item {
+    padding-left: 40px;
+}
+
+.list-group a.list-group-item {
+    cursor: pointer;
+}
+
+#group-list {
+    overflow: auto;
+}
+
+#group-list a {
+    display: block;
+}
+
+#group-list .list-group,
+#group-list .list-group .list-group-item {
+    border-radius: 0;
+}
+
+#group-list + .panel-footer {
+    border-top: 1px solid #ddd
+}
+
+.placeholder-text {
+    font-style: italic;
+    color: #aaa;
+}
+
+.tier-select {
+    width: 300px;
+}
+
+.storage-table {
+    margin-bottom: 0px!important;
+}
--- stats/stats.py
+++ stats/stats.py
@@ -3,13 +3,49 @@
 __copyright__ = 'Copyright (c) 2021, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
-from flask import Blueprint, render_template
+from datetime import datetime
+
+from flask import Blueprint, make_response, render_template
+
+import api
 
 stats_bp = Blueprint('stats_bp', __name__,
                      template_folder='templates/stats',
-                     static_folder='static/stats')
+                     static_folder='static/stats',
+                     static_url_path='/static')
 
 
 @stats_bp.route('/')
 def index():
-    return render_template('stats.html')
+    group_response = api.call('resource_list_groups', data={})
+    category_response = api.call('resource_category_stats', data={})
+    return render_template('stats.html',
+                           groups=group_response['data'],
+                           categories=category_response['data'])
+
+
+@stats_bp.route('/export')
+def export():
+    response = api.call('resource_monthly_category_stats', data={})
+
+    csv = "category;subcategory;groupname;tier;"
+
+    months = ['January', 'February', 'March', 'April',
+              'May', 'June', 'July', 'August',
+              'September', 'October', 'November', 'December']
+    current_month = datetime.now().month
+    for i in range(11, -1, -1):
+        month = (current_month - i) + 12 if (current_month - i) < 0 else current_month - i
+        csv += "{};".format(months[month - 1])
+    csv += "\n"
+
+    for stat in response['data']:
+        csv += "{};{};{};{};".format(stat['category'], stat['subcategory'], stat['groupname'], stat['tier'])
+        for month in stat['storage']:
+            csv += "{};".format(month)
+        csv += "\n"
+
+    output = make_response(csv)
+    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
