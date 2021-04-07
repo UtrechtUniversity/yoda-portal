5764946f5c3d951d6a2460718e1af904d76fc990
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-04-07 12:33:47 +0200

    Add home button to error pages.

--- general/general.py
+++ general/general.py
@@ -5,8 +5,6 @@ __license__   = 'GPLv3, see LICENSE'
 
 from flask import Blueprint, render_template
 
-import api
-
 general_bp = Blueprint('general_bp', __name__,
                        template_folder='templates/general',
                        static_folder='static/general',
@@ -20,11 +18,9 @@ def index():
 
 @general_bp.app_errorhandler(404)
 def page_not_found(e):
-    """ Return error 404 """
     return render_template('404.html'), 404
 
 
 @general_bp.app_errorhandler(500)
 def internal_error(e):
-    """ Return error 500 """
     return render_template('500.html'), 500
