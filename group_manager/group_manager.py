853697b5366e961f51a9b1da983da740f49ac107
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-04-07 15:54:40 +0200

    Build group list from API response.

--- group_manager/group_manager.py
+++ group_manager/group_manager.py
@@ -3,7 +3,11 @@
 __copyright__ = 'Copyright (c) 2021, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
+import json
+
 from flask import Blueprint, render_template
+
+import api
 
 group_bp = Blueprint('group_bp', __name__,
                      template_folder='templates',
@@ -13,11 +17,11 @@ group_bp = Blueprint('group_bp', __name__,
 
 @group_bp.route('/example')
 def index():
-    group_response = api.call('group_data', data={})
-    
-    group_hierarchy = {}
-    user_type = "rodsuser"
-    user_zone = "tempZone"
+    response = api.call('group_data', data={})
+
+    group_hierarchy = response['data']['group_hierarchy']
+    user_type = response['data']['user_type']
+    user_zone = response['data']['user_zone']
 
     return render_template('group_manager/index.html',
                             group_hierarchy=group_hierarchy,
