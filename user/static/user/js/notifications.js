fd09bc8763ad2cf528fab305442c31099b2efa95
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-04-12 12:48:35 +0200

    Don't go to link when marking notification as done.

--- user/static/user/js/notifications.js
+++ user/static/user/js/notifications.js
@@ -1,7 +1,8 @@
 "use strict";
 
 $(function() {
-    $("body").on("click", "h5.dismiss-notification", function() {
+    $("body").on("click", "a.list-group-item > h5.dismiss-notification", function(e) {
+        e.preventDefault();
         let identifier = $(this).attr('data-id');
         $(this).closest('a.list-group-item').remove();
         Yoda.call('notifications_dismiss', {identifier: identifier});
