3c90b814e75f77d80e7c68a3870da735883d6837
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-04-12 11:29:44 +0200

    Fix marking notification as done.

--- user/static/user/js/notifications.js
+++ user/static/user/js/notifications.js
@@ -1,9 +1,9 @@
 "use strict";
 
 $(function() {
-    $("body").on("click", "a.dismiss-notification", function() {
+    $("body").on("click", "h5.dismiss-notification", function() {
         let identifier = $(this).attr('data-id');
-        $(this).closest('tr').remove();
+        $(this).closest('a.list-group-item').remove();
         Yoda.call('notifications_dismiss', {identifier: identifier});
     });
 });
