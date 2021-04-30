8835bbdd01a5aacfc22a3618bfd86fe2653a7738
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-04-30 17:01:17 +0200

    Disable mail notification type selection when mail notifications are disabled.

--- user/static/user/js/settings.js
+++ user/static/user/js/settings.js
@@ -1,3 +1,12 @@
 "use strict";
 
-$(function() {});
+$(function() {
+    $("body").on("click", ".custom-switch", function(e) {
+        let type = $("#mail_notifications_type")
+        if($('#mail_notifications').prop('checked')) {
+            type.prop('disabled', false);
+        } else {
+            type.prop('disabled', true);
+        }
+    });
+});
