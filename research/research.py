01067f958b0f336c6efaa6e0bd94ff296ece0f91
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-05-11 14:00:45 +0200

    Restore basic upload functionality.

--- research/research.py
+++ research/research.py
@@ -100,6 +100,28 @@ def download():
         abort(404)
 
 
+@research_bp.route('/browse/upload', methods=['POST'])
+def upload():
+    try:
+        import os
+        from werkzeug.utils import secure_filename
+
+        filepath = request.form.get('filepath')
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        tmp_path = os.path.join("/var/www/yoda/uploads", filename)
+        file.save(tmp_path)
+
+        path = '/' + g.irods.zone + '/home' + filepath + "/" + filename
+        session = g.irods
+        session.data_objects.put(tmp_path, path)
+
+        os.remove(tmp_path)
+        return {"status": "OK", "statusInfo": ""}
+    except Exception:
+        return {"status": "ERROR", "statusInfo": "Upload failed"}
+
+
 @research_bp.route('/revision')
 def revision():
     items = 10
