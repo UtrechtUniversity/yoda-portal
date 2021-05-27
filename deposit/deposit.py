7bc328b5a1a7da9ff42b7e8ab0a9388cb1cbbeed
Joris de Ruiter <j.deruiter2@uu.nl>
2021-05-27 13:12:46 +0200

    upload copy from research

--- deposit/deposit.py
+++ deposit/deposit.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from flask import Blueprint, render_template, redirect, url_for
 
 __copyright__ = 'Copyright (c) 2021, Utrecht University'
 __license__ = 'GPLv3, see LICENSE'
+
+import os
+from flask import Blueprint, render_template, redirect, url_for, g, jsonify, make_response, request, session
 
 deposit_bp = Blueprint('deposit_bp', __name__,
                        template_folder='templates/deposit',
@@ -12,9 +14,9 @@ deposit_bp = Blueprint('deposit_bp', __name__,
 
 """
 Deposit flow:
-1. upload data:     /deposit/
-2. add metadata:    /deposit/metadata/
-3. submit:          /deposit/submit/
+1. Upload data:     /deposit/
+2. Add metadata:    /deposit/metadata/
+3. Submit:          /deposit/submit/
 """
 
 
@@ -47,6 +49,142 @@ def submit():
 
 
 @deposit_bp.route('/submit', methods=['POST'])
-def submit_handler():
+def submit():
     """ Step 3: Submit upload """
     return render_template('thankyou.html')
+
+
+
+@deposit_bp.route('/prototype_upload')
+def prototype_upload():
+    return render_template('deposit/upload.html')
+
+
+def get_chunk_name(uploaded_filename, chunk_number):
+    return uploaded_filename + "_part_%03d" % chunk_number
+
+
+@deposit_bp.route('/flow_upload', methods=['GET'])
+def flow_upload_get():
+    flow_identifier = request.args.get('flowIdentifier', type=str)
+    flow_filename = request.args.get('flowFilename', type=str)
+    flow_chunk_number = request.args.get('flowChunkNumber', type=int)
+
+    filepath = request.args.get('filepath', type=str)
+
+    if not flow_identifier or not flow_filename or not flow_chunk_number or not filepath:
+        # Parameters are missing or invalid.
+        response = make_response(jsonify({"message": "Parameter missing or invalid"}), 500)
+        response.headers["Content-Type"] = "application/json"
+        return response
+
+    # Build chunk folder path based on the parameters.
+    temp_dir = os.path.join("/" + g.irods.zone, 'home', filepath, flow_identifier)
+
+    # Chunk path based on the parameters.
+    chunk_path = os.path.join(temp_dir, get_chunk_name(flow_filename, flow_chunk_number))
+
+    session = g.irods
+    if session.data_objects.exists(chunk_path):
+        # Chunk already exists.
+        response = make_response(jsonify({"message": "Chunk found"}), 200)
+        response.headers["Content-Type"] = "application/json"
+        return response
+    else:
+        # Chunk does not exists and needs to be uploaded.
+        response = make_response(jsonify({"message": "Chunk not found"}), 204)
+        response.headers["Content-Type"] = "application/json"
+        return response
+
+
+@deposit_bp.route('/flow_upload', methods=['POST'])
+def flow_upload_post():
+    flow_identifier = request.form.get('flowIdentifier', type=str)
+    flow_filename = request.form.get('flowFilename', type=str)
+    flow_chunk_number = request.form.get('flowChunkNumber', type=int)
+    flow_total_chunks = request.form.get('flowTotalChunks', type=int)
+    flow_chunk_size = request.form.get('flowChunkSize', type=int)
+    flow_relative_path = request.form.get('flowRelativePath', type=str)
+
+    relative_path = os.path.dirname(flow_relative_path)
+    filepath = request.form.get('filepath', type=str)
+
+    if (not flow_identifier or not flow_filename or not flow_chunk_number
+       or not flow_total_chunks or not flow_chunk_size or not filepath):
+        # Parameters are missing or invalid.
+        response = make_response(jsonify({"message": "Parameter missing or invalid"}), 500)
+        response.headers["Content-Type"] = "application/json"
+        return response
+
+    session = g.irods
+
+    # Ensure temp chunk collection exists.
+    if relative_path:
+        base_dir = os.path.join("/" + g.irods.zone, 'home', filepath, relative_path)
+        if not session.collections.exists(base_dir):
+            session.collections.create(base_dir)
+    else:
+        base_dir = os.path.join("/" + g.irods.zone, 'home', filepath)
+
+    # Get the chunk data.
+    chunk_data = request.files['file']
+
+    if flow_total_chunks == 1:
+        file_path = os.path.join(base_dir, flow_filename)
+
+        try:
+            obj = session.data_objects.create(file_path)
+            with obj.open('w+') as f:
+                f.write(chunk_data.stream.read())
+            f.close()
+        except Exception:
+            response = make_response(jsonify({"message": "Chunk upload failed"}), 500)
+            response.headers["Content-Type"] = "application/json"
+            return response
+    else:
+        # Ensure temp chunk collection exists.
+        temp_dir = os.path.join(base_dir, flow_identifier)
+        if not session.collections.exists(temp_dir):
+            session.collections.create(temp_dir)
+
+        # Save the chunk data.
+        chunk_path = os.path.join(temp_dir, get_chunk_name(flow_filename, flow_chunk_number))
+        try:
+            obj = session.data_objects.create(chunk_path)
+
+            with obj.open('w+') as f:
+                f.write(chunk_data.stream.read())
+            f.close()
+        except Exception:
+            response = make_response(jsonify({"message": "Chunk upload failed"}), 500)
+            response.headers["Content-Type"] = "application/json"
+            return response
+
+        # Check if the upload is complete.
+        chunk_paths = [os.path.join(temp_dir, get_chunk_name(flow_filename, x)) for x in range(1, flow_total_chunks + 1)]
+        upload_complete = all([session.data_objects.exists(p) for p in chunk_paths])
+
+        # Combine all the chunks to create the final file.
+        if upload_complete:
+            file_path = os.path.join(base_dir, flow_filename)
+
+            obj = session.data_objects.create(file_path, force=True)
+
+            with obj.open('w+') as f:
+                chunk_number = 0
+                for chunk in chunk_paths:
+                    # read file
+                    obj2 = session.data_objects.get(chunk)
+                    with obj2.open('r') as f2:
+                        f.seek(chunk_number * flow_chunk_size)
+                        f.write(f2.read())
+                    f2.close()
+                    chunk_number += 1
+
+                coll = session.collections.get(temp_dir)
+                coll.remove(recurse=True, force=True)
+            f.close()
+
+    response = make_response(jsonify({"message": "Chunk upload succeeded"}), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
---
+++ deposit/static/deposit/js/upload.js
@@ -0,0 +1,127 @@
+$(function() {
+    console.log('ready');
+    var r = new Flow({
+        target: '/deposit/flow_upload',
+        chunkSize: 10 * 1024 * 1024,
+        simultaneousUploads: 5,
+        query: {'csrf_token': Yoda.csrf.tokenValue, 'filepath': 'research-initial'}
+    });
+    // Flow.js isn't supported, fall back on a different method
+    if (!r.support) {
+        Yoda.set_message('error', 'No browser support for Flow file uploads');
+        return ;
+    }
+
+    // Show a place for dropping/selecting files
+    $('.flow-drop').show();
+    r.assignDrop($('.flow-drop')[0]);
+    r.assignBrowse($('.flow-browse')[0]);
+    r.assignBrowse($('.flow-browse-folder')[0], true);
+
+    // Handle file add event
+    r.on('fileAdded', function(file){
+        console.log(file);
+        // Show progress bar
+        $('.flow-progress, .flow-list').show();
+        // Add the file to the list
+        $('.flow-list').append(
+            '<li class="flow-file flow-file-'+file.uniqueIdentifier+'">' +
+            'Uploading <span class="flow-file-name"></span> ' +
+            '<span class="flow-file-size"></span> ' +
+            '<span class="flow-file-progress"></span> ' +
+            '<span class="flow-file-pause">' +
+            '<u>Pause upload</u>' +
+            '</span>' +
+            '<span class="flow-file-resume">' +
+            '<u>Resume upload</u>' +
+            '</span>' +
+            '<span class="flow-file-cancel">' +
+            '<u>Cancel upload</u>' +
+            '</span>'
+        );
+        var $self = $('.flow-file-'+file.uniqueIdentifier);
+        $self.find('.flow-file-name').text(file.relativePath);
+        $self.find('.flow-file-size').text(readablizeBytes(file.size));
+        $self.find('.flow-file-pause').on('click', function () {
+            file.pause();
+            $self.find('.flow-file-pause').hide();
+            $self.find('.flow-file-resume').show();
+        });
+        $self.find('.flow-file-resume').on('click', function () {
+            file.resume();
+            $self.find('.flow-file-pause').show();
+            $self.find('.flow-file-resume').hide();
+        });
+        $self.find('.flow-file-cancel').on('click', function () {
+            file.cancel();
+            $self.remove();
+        });
+    });
+    r.on('filesSubmitted', function(file) {
+        r.upload();
+    });
+    r.on('complete', function(){
+        // Hide pause/resume when the upload has completed
+        $('.flow-progress .progress-resume-link, .flow-progress .progress-pause-link').hide();
+    });
+    r.on('fileSuccess', function(file,message){
+        var $self = $('.flow-file-'+file.uniqueIdentifier);
+        // Reflect that the file upload has completed
+        $self.find('.flow-file-progress').text('(completed)');
+        $self.find('.flow-file-pause, .flow-file-resume').remove();
+    });
+    r.on('fileError', function(file, message){
+        // Reflect that the file upload has resulted in error
+        $('.flow-file-'+file.uniqueIdentifier+' .flow-file-progress').html('(file could not be uploaded: '+message+')');
+    });
+    r.on('fileProgress', function(file){
+        // Handle progress for both the file and the overall upload
+        $('.flow-file-'+file.uniqueIdentifier+' .flow-file-progress')
+            .html(Math.floor(file.progress()*100) + '% '
+                + readablizeBytes(file.averageSpeed) + '/s '
+                + secondsToStr(file.timeRemaining()) + ' remaining') ;
+        $('.progress-bar').css({width:Math.floor(r.progress()*100) + '%'});
+    });
+    r.on('uploadStart', function(){
+        // Show pause, hide resume
+        $('.flow-progress .progress-resume-link').hide();
+        $('.flow-progress .progress-pause-link').show();
+    });
+    r.on('catchAll', function() {
+        console.log.apply(console, arguments);
+    });
+    /*
+    r.on('catchAll', function() {
+        console.log.apply(console, arguments);
+    });
+     */
+});
+
+function readablizeBytes(bytes) {
+    var s = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
+    var e = Math.floor(Math.log(bytes) / Math.log(1024));
+    return (bytes / Math.pow(1024, e)).toFixed(2) + " " + s[e];
+}
+function secondsToStr (temp) {
+    function numberEnding (number) {
+        return (number > 1) ? 's' : '';
+    }
+    var years = Math.floor(temp / 31536000);
+    if (years) {
+        return years + ' year' + numberEnding(years);
+    }
+    var days = Math.floor((temp %= 31536000) / 86400);
+    if (days) {
+        return days + ' day' + numberEnding(days);
+    }
+    var hours = Math.floor((temp %= 86400) / 3600);
+    if (hours) {
+        return hours + ' hour' + numberEnding(hours);
+    }
+    var minutes = Math.floor((temp %= 3600) / 60);
+    if (minutes) {
+        return minutes + ' minute' + numberEnding(minutes);
+    }
+    var seconds = temp % 60;
+    return seconds + ' second' + numberEnding(seconds);
+}
