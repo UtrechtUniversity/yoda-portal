2f48466b457af506da99ebeac38826872c909b9e
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-07-07 10:55:59 +0200

    Move submit javascript to seperate file.

---
+++ deposit/static/deposit/js/deposit-submit.js
@@ -0,0 +1,26 @@
+"use strict";
+
+$(document).ajaxSend(function(e, request, settings) {
+    // Append a CSRF token to all AJAX POST requests.
+    if (settings.type === 'POST' && settings.data.length) {
+         settings.data
+             += '&' + encodeURIComponent(Yoda.csrf.tokenName)
+              + '=' + encodeURIComponent(Yoda.csrf.tokenValue);
+    }
+});
+
+$(function() {
+    $("body").on("click", "button#submit", function() {
+        submitToVault();
+    });
+});
+
+async function submitToVault()
+{
+    try {
+        let status = await Yoda.call('deposit_submit', {})
+        console.log(status);
+    } catch (e) {
+        console.log(e);
+    }
+}
