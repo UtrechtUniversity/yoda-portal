eece62c652c48557e1f44d2d02b7998b941d0da6
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-07-08 10:39:28 +0200

    Fix enabling submit button when deposit is ready.

--- deposit/static/deposit/js/deposit-submit.js
+++ deposit/static/deposit/js/deposit-submit.js
@@ -17,11 +17,19 @@ $(function() {
     });
 
     $("#accept_terms").change(function() {
-        if($(this).checked && getStatus()) {
-            $("#submit").prop('disabled', false);
-        }
+        submitStatus();
     });
 });
+
+async function submitStatus()
+{
+    let status = await getStatus();
+    if($("#accept_terms").is(':checked') && status) {
+        $("#submit").prop('disabled', false);
+    } else {
+        $("#submit").prop('disabled', true);
+    }
+}
 
 async function getStatus()
 {
@@ -29,10 +37,15 @@ async function getStatus()
         let status = await Yoda.call('deposit_status', {})
         if (status.data) {
             $('#data_check').removeClass('fa-times text-danger').addClass('fa-check text-success');
+        } else {
+            $('#data_check').removeClass('fa-check text-success').addClass('fa-times text-danger');
         }
         if (status.metadata) {
             $('#metadata_check').removeClass('fa-times text-danger').addClass('fa-check text-success');
+        } else {
+            $('#metadata_check').removeClass('fa-check text-success').addClass('fa-times text-danger');
         }
+
         if (status.data && status.metadata) {
             return true;
         } else {
