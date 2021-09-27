dc44f676a5123e0d2811137dcfc3801a93c4d213
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-09-27 15:19:39 +0200

    Add feedback messages to data access password generation.

--- user/static/user/js/data_access.js
+++ user/static/user/js/data_access.js
@@ -16,18 +16,15 @@ $(document).ready(function() {
 
     $("body").on("click", "a.gen-token", function(e){
         let label = $("#f-token-label").val();
+        $('a.gen-token').hide();
         Yoda.call("generate_token", {"label": label}, {"quiet": true}).then(
             (data) => {
                 $('#f-token').val(data);
-                $('a.gen-token').hide();
-                // Please store the password somewhere safe, you will not be able to do so after you close this window!
+                $('#passwordOk').show();
             },
             (error) => {
-                Yoda.set_message(
-                    'error',
-                    `An error occurred while creating the token,
-please try another label.
-If the issue persists, contact your administrator`);
+                $('a.gen-token').show();                
+                $('#passwordError').show();
             });
     });
 });
