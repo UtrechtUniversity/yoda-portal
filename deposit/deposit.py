52c1ccb651726663eafc3d3c9eb5c2a0587674aa
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-11-08 14:05:47 +0100

    Add deposit group for now.

--- deposit/deposit.py
+++ deposit/deposit.py
@@ -3,7 +3,7 @@
 __copyright__ = 'Copyright (c) 2021, Utrecht University'
 __license__ = 'GPLv3, see LICENSE'
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 import api
 
@@ -32,10 +32,8 @@ def get_deposit_path():
 @deposit_bp.route('/browse')
 def index():
     """Deposit overview"""
-
     items = 25
-    # path = request.args.get('dir')
-    path = get_deposit_path()
+    path = "/deposit-pilot"
 
     return render_template('deposit/overview.html',
                            activeModule='deposit',
@@ -47,10 +45,8 @@ def index():
 @deposit_bp.route('/data')
 def data():
     """Step 1: Add data"""
-
     items = 25
-    # path = request.args.get('dir')
-    path = get_deposit_path()
+    path = request.args.get('dir', get_deposit_path())
 
     return render_template('deposit/data.html',
                            activeModule='deposit',
@@ -62,19 +58,19 @@ def data():
 @deposit_bp.route('/metadata')
 def metadata():
     """Step 2: Document data"""
-    path = get_deposit_path()
+    path = request.args.get('dir')
     return render_template('deposit/metadata-form.html', path=path)
 
 
 @deposit_bp.route('/submit')
 def submit():
     """Step 3: Submit data"""
-    path = get_deposit_path()
+    path = request.args.get('dir')
     return render_template('deposit/submit.html', path=path)
 
 
 @deposit_bp.route('/thank-you')
 def thankyou():
     """Step 4: Thank you"""
-    path = get_deposit_path()
+    path = request.args.get('dir')
     return render_template('deposit/thank-you.html', path=path)
