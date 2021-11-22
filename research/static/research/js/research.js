0419c535f101bfbe89322f55d261eb353635af59
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-11-22 15:44:01 +0100

    Add action to stage offline data object.

--- research/static/research/js/research.js
+++ research/static/research/js/research.js
@@ -684,26 +684,31 @@ const tableRenderer = {
             actions.append(`<a href="#" class="dropdown-item folder-delete" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Delete this file">Delete</a>`);
         }
         else {
-            // Render context menu for files.
-            const viewExts = {
-                image: ['jpg', 'jpeg', 'gif', 'png'],
-                audio: ['mp3', 'ogg', 'wav'],
-                video: ['mp4', 'ogg', 'webm']
-            };
-            let ext = row.name.replace(/.*\./, '').toLowerCase();
+            if (row.status === 'OFFLINE') {
+                actions.append(`<a href="#" class="dropdown-item file-stage" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Bring this file online">Bring online</a>`);
+            } else {
+                // Render context menu for files.
+                const viewExts = {
+                    image: ['jpg', 'jpeg', 'gif', 'png'],
+                    audio: ['mp3', 'ogg', 'wav'],
+                    video: ['mp4', 'ogg', 'webm']
+                };
+                let ext = row.name.replace(/.*\./, '').toLowerCase();
 
-            actions.append(`<a class="dropdown-item" href="browse/download?filepath=${encodeURIComponent(currentFolder + '/' + row.name)}" title="Download this file">Download</a>`);
+                actions.append(`<a class="dropdown-item" href="browse/download?filepath=${encodeURIComponent(currentFolder + '/' + row.name)}" title="Download this file">Download</a>`);
 
-            // Generate dropdown "view" actions for different media types.
-            for (let type of Object.keys(viewExts).filter(type => (viewExts[type].includes(ext)))) {
-                actions.append(`<a class="dropdown-item view-${type}" data-path="${htmlEncode(currentFolder + '/' + row.name)}" title="View this file">View</a>`);
+                // Generate dropdown "view" actions for different media types.
+                for (let type of Object.keys(viewExts).filter(type => (viewExts[type].includes(ext)))) {
+                    actions.append(`<a class="dropdown-item view-${type}" data-path="${htmlEncode(currentFolder + '/' + row.name)}" title="View this file">View</a>`);
+                }
+
+                actions.append(`<a href="#" class="dropdown-item file-rename" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Rename this file">Rename</a>`);
+                actions.append(`<a href="#" class="dropdown-item file-copy" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Copy this file">Copy</a>`);
+                actions.append(`<a href="#" class="dropdown-item file-move" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Move this file">Move</a>`);
+                actions.append(`<a href="#" class="dropdown-item file-delete" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Delete this file">Delete</a>`);
             }
-
-            actions.append(`<a href="#" class="dropdown-item file-rename" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Rename this file">Rename</a>`);
-            actions.append(`<a href="#" class="dropdown-item file-copy" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Copy this file">Copy</a>`);
-            actions.append(`<a href="#" class="dropdown-item file-move" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Move this file">Move</a>`);
-            actions.append(`<a href="#" class="dropdown-item file-delete" data-collection="${htmlEncode(currentFolder)}" data-name="${htmlEncode(row.name)}" title="Delete this file">Delete</a>`);
         }
+
         let dropdown = $(`<div class="dropdown">
                             <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-name="${htmlEncode(row.name)}" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
