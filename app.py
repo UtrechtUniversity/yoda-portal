0e6f0fc7673e56b25392ee356ced7c546c927580
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-12-22 13:45:03 +0100

    Fix CSP for OIDC gate in Chrome.

--- app.py
+++ app.py
@@ -164,6 +164,9 @@ def content_security_policy(response):
     """Add Content-Security-Policy headers."""
     if request.endpoint in ['deposit_bp.metadata', 'research_bp.form', 'vault_bp.form']:
         response.headers['Content-Security-Policy'] = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' data: *.openstreetmap.org; frame-ancestors 'self'; form-action 'self'; object-src 'none'"  # noqa: E501
+    elif request.endpoint in ['user_bp.gate']:
+        response.headers['Content-Security-Policy'] = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; frame-ancestors 'self'; form-action 'self' https:; object-src 'none'"  # noqa: E501
     else:
         response.headers['Content-Security-Policy'] = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; frame-ancestors 'self'; form-action 'self'; object-src 'none'"  # noqa: E501
+
     return response
