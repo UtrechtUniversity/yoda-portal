335475095c0fdc98ae170dcc7f9deb588440306e
Harm de Raaff <harm.apple@gmail.com>
2022-01-25 16:59:36 +0100

    Introduced proper handling with irods function uuGroupAdd() and limit groupname length to 63 max

--- group_manager/group_manager.py
+++ group_manager/group_manager.py
@@ -80,7 +80,7 @@ def group_create():
                                               'description': request.form['group_description'],
                                               'data_classification': data_classification})
 
-    output = make_response({'status': 0 if response['status'] == 'ok' else 1, 'message': response['status_info']})
+    output = make_response({'status': 0 if response['data']['proc_status'] == '0' else 1, 'message': response['data']['proc_status_info']})
     output.headers["Content-type"] = "application/json"
     return output
 
