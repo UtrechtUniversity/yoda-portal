f01ab4cd5ccf95b475bdc4f86de597ec2a942dd8
Harm de Raaff <harm.apple@gmail.com>
2022-04-05 08:12:44 +0200

    draggable overview, better initially placed

--- deposit/static/deposit/js/data.js
+++ deposit/static/deposit/js/data.js
@@ -250,8 +250,45 @@ $(function() {
             $("#file-browser_paginate").hide();
         }
     });
+
+    dragElement(document.getElementById("uploads"));
+
 });
 
+// draggability of the upload overview div
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById("uploads_header")) {
+    document.getElementById("uploads_header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 async function handleFolderAdd(new_folder, collection) {
 
