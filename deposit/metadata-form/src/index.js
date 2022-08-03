10205c05d9999d44f0f6e1a0efb05e2df67e60ad
Lazlo Westerhof <l.r.westerhof@uu.nl>
2022-08-03 15:50:54 +0200

    Prevent triggering buttons in ArrayFieldTemplate on enter (fixes UtrechtUniversity/yoda#134).

--- deposit/metadata-form/src/index.js
+++ deposit/metadata-form/src/index.js
@@ -606,7 +606,7 @@ function ArrayFieldTemplate(props) {
                                 <div className="d-flex flex-row">
                                     {el.hasMoveUp && (
                                         <div className="m-0 p-0">
-                                            <button className="btn btn-light btn-sm"
+                                            <button className="btn btn-light btn-sm" type="button" tabindex="-1"
                                                     onClick={el.onReorderClick(
                                                         el.index,
                                                         el.index - 1
@@ -618,7 +618,7 @@ function ArrayFieldTemplate(props) {
 
                                     {el.hasMoveDown && (
                                         <div className="m-0 p-0">
-                                            <button className="btn btn-light btn-sm"
+                                            <button className="btn btn-light btn-sm" type="button" tabindex="-1"
                                                     onClick={el.onReorderClick(
                                                         el.index,
                                                         el.index + 1
@@ -630,7 +630,7 @@ function ArrayFieldTemplate(props) {
 
                                     {el.hasRemove && (
                                         <div className="m-0 p-0">
-                                            <button className="btn btn-light btn-sm"
+                                            <button className="btn btn-light btn-sm" type="button" tabindex="-1"
                                                     onClick={el.onDropIndexClick(el.index)}>
                                                 <i className="fa-solid fa-trash" aria-hidden="true"></i>
                                             </button>
--- research/metadata-form/src/index.js
+++ research/metadata-form/src/index.js
@@ -634,7 +634,7 @@ function ArrayFieldTemplate(props) {
                                 <div className="d-flex flex-row">
                                     {el.hasMoveUp && (
                                         <div className="m-0 p-0">
-                                            <button className="btn btn-light btn-sm"
+                                            <button className="btn btn-light btn-sm" type="button" tabindex="-1"
                                                     onClick={el.onReorderClick(
                                                         el.index,
                                                         el.index - 1
@@ -646,7 +646,7 @@ function ArrayFieldTemplate(props) {
 
                                     {el.hasMoveDown && (
                                         <div className="m-0 p-0">
-                                            <button className="btn btn-light btn-sm"
+                                            <button className="btn btn-light btn-sm" type="button" tabindex="-1"
                                                     onClick={el.onReorderClick(
                                                         el.index,
                                                         el.index + 1
@@ -658,7 +658,7 @@ function ArrayFieldTemplate(props) {
 
                                     {el.hasRemove && (
                                         <div className="m-0 p-0">
-                                            <button className="btn btn-light btn-sm"
+                                            <button className="btn btn-light btn-sm" type="button" tabindex="-1"
                                                     onClick={el.onDropIndexClick(el.index)}>
                                                 <i className="fa-solid fa-trash" aria-hidden="true"></i>
                                             </button>
