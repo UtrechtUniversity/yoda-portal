fc5b48a48cb493308369b301aabeb7f5b7d8df88
Roy van Elk <github@royvanelk.nl>
2022-11-07 14:02:18 +0100

    Add vocabulary field

---
+++ research/metadata-form/src/Vocabulary.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Select from 'react-select';
+import axios from "axios";
+
+class Vocabulary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props.formData,
+            options: "",
+            placeholder: props.uiSchema['ui:default']
+        };
+
+        const url = props.uiSchema['ui:data'];
+
+        const loadData = async () => {
+            const arr = [];
+            await axios.get(url).then((res) => {
+                let result = res.data;
+                result.map((item) => {
+                    return arr.push({value: item.value, label: item.label});
+                });
+                // Fill options data
+                this.setState({ options: arr });
+
+                // Find the label by the form data key
+                let option = arr.find(o => o.value === this.props.formData);
+                if (option !== undefined) {
+                    this.setState({ placeholder: option.label });
+                }
+            });
+        };
+        loadData();
+    }
+
+    render() {
+        let title = this.props.schema.title || this.props.uiSchema["ui:title"];
+
+        return (
+            <div>
+                <label className="form-label">{title}</label>
+                <Select
+                    className={'select-box'}
+                    options={this.state.options}
+                    placeholder={this.state.placeholder}
+                    onChange={(event) => this.props.onChange(event.value)}/>
+            </div>
+        );
+    }
+}
+
+export default Vocabulary;
