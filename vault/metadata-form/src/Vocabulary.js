fb6154dd004bb696db43d83fa9850ff31aac6d66
Lazlo Westerhof <l.r.westerhof@uu.nl>
2022-12-07 14:59:03 +0100

    Add support for required indicator to vocabulary fields.

--- vault/metadata-form/src/Vocabulary.js
+++ vault/metadata-form/src/Vocabulary.js
@@ -36,17 +36,48 @@ class Vocabulary extends React.Component {
 
     render() {
         let title = this.props.schema.title || this.props.uiSchema["ui:title"];
+        let label = <label className="form-label">{title}</label>
         let help = this.props.uiSchema["ui:help"];
+        let customStyles = {
+            control: styles => ({
+                ...styles,
+                border: '1px solid #ced4da',
+                boxShadow: 'none',
+                '&:hover': {
+                    border: '1px solid #ced4da',
+                }
+            })
+        };
+        let required = this.props.required
+        let error = "should be equal to one of the allowed values";
+
+        if((this.props.rawErrors !== undefined && this.props.rawErrors.indexOf(error) >= 0) || (required && this.props.formData == null)) {
+            label = <label className="text-danger form-label select-required">{title}*</label>
+            customStyles = {
+                control: styles => ({
+                    ...styles,
+                    border: '1px solid #dc3545',
+                    boxShadow: 'none',
+                    '&:hover': {
+                        border: '1px solid #dc3545',
+                    }
+                })
+            };
+        } else if (required) {
+            label = <label className="form-label select-required select-filled">{title}*</label>
+        }
 
         return (
             <div>
-                <label className="form-label">{title}</label>
+                {label}
                 <Select
                     className={'select-box'}
                     options={this.state.options}
+                    required={required}
                     isDisabled={this.props.readonly}
                     placeholder={this.state.placeholder}
-                    onChange={(event) => this.props.onChange(event.value)} />
+                    onChange={(event) => this.props.onChange(event.value)}
+                    styles={customStyles} />
                 {help && (
                     <small className="text-muted form-text">
                         <p className="help-block">{help}</p>
