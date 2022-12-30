657005d159f46ce6dc389cdfebec516734ac1c7d
Harm de Raaff <harm.apple@gmail.com>
2022-12-30 08:05:43 +0100

    use datatables including orderering for view on groups

--- stats/static/stats/js/stats.js
+++ stats/static/stats/js/stats.js
@@ -19,10 +19,9 @@ $(document).ready(function() {
         getDetails($(this).attr('data-name'));
     });
 
-    $(".list-group-item.group").click(function() {
-        makeItemActive($(this));
-        getGroupDetails($(this).attr('data-name'));
-    });
+    if ($('#group-browser').length) {
+        startBrowsing(10);
+    }
 });
 
 function makeItemActive(currentItem)
@@ -222,3 +221,167 @@ function htmlDecode(inp){
         return String.fromCharCode(dec);
     });
 }
+
+
+
+function startBrowsing(pageLength)
+{
+    $('#group-browser').DataTable({
+        "bFilter": false,
+        "bInfo": false,
+        "bLengthChange": true,
+        "language": {
+            "emptyTable": "No group information present.",
+            "lengthMenu": "_MENU_"
+        },
+        "dom": '<"top">frt<"bottom"lp><"clear">',
+        'columns': [//{render: tableRenderer.multiselect,    orderable: false, data: 'name'},
+                    {render: tableRenderer.name,    data: 'name', searchable: true},
+                    // Size and date should be orderable, but limitations
+                    // on how queries work prevent us from doing this
+                    // correctly without significant overhead.
+                    // (enabling this as is may result in duplicated results for data objects)
+                    {render: tableRenderer.size,    data: 'size'}],
+                    //{render: tableRenderer.date,    orderable: false, data: 'modify_time'},
+                    //{render: tableRenderer.state,   orderable: false},
+                    //{render: tableRenderer.context, orderable: false }],
+        "ajax": getFolderContents,
+        "processing": true,
+        "serverSide": true,
+        "iDeferLoading": 0,
+        "order": [[ 0, "asc" ]],
+        "pageLength": pageLength,
+//        "searching": true,
+        "fnDrawCallback": function() {
+             $("#group-browser td").click(function() {
+                 var groupName = $(this).parent().find('.list-group-item').attr('data-name');
+                 getGroupDetails(groupName);
+                 $('#selected-group').html('Group [' + groupName + ']');
+                 //$('#group-browser').DataTable().search( 'pilot' ).draw();
+             });
+        }
+    });
+    browse();
+}
+
+
+function browse()
+{
+    buildFileBrowser();
+}
+
+
+function buildFileBrowser() {
+    let groupBrowser = $('#group-browser').DataTable();
+    getFolderContents.dropCache();
+    groupBrowser.ajax.reload();
+
+    return true;
+}
+
+// rendering part
+
+// getFolderContents
+// Fetches directory contents to populate the listing table.
+let getFolderContents = (() => {
+    // Close over some state variables.
+    // -> we keep a multi-page cache handy, since getting only $page_length [=10]
+    //    results each time is wasteful and slow.
+    // A change in sort column/order or folder will invalidate the cache.
+
+    // The amount of rows to request at once.
+    // *Must* be equal to or greater than the largest datatables page length,
+    // and *should* be smaller than iRODS SQL rows per batch.
+    const batchSize = 200;
+    // (~140 B per entry in JSON returned by iRODS,
+    //  so depending on name = up to 28K to transfer for each fetch)
+
+    let total          = false; // Total subcollections / data objects.
+    let cache          = [];    // Cached result rows (may be more than shown on one page).
+    let cacheStart     = null;  // Row number of the first cache entry.
+    let cacheSortCol   = null;  // Cached sort column nr.
+    let cacheSortOrder = null;  // Cached sort order.
+    let i = 0;                  // Keep simultaneous requests from interfering.
+
+    let get = async (args) => {
+        // Check if we can use the cache.
+        if (cache.length
+         && args.order[0].dir    === cacheSortOrder
+         && args.order[0].column === cacheSortCol
+         && args.start               >= cacheStart
+         && args.start + args.length <= cacheStart + batchSize) {
+
+            return cache.slice(args.start - cacheStart, args.start - cacheStart + args.length);
+        } else {
+            // Nope, load new data via the API.
+            let j = ++i;
+            let result = await Yoda.call('browse_group_data', // 'browse_folder',
+                                         {'offset':     args.start,
+                                          'limit':      batchSize,
+                                          'sort_order': args.order[0].dir,
+                                          'sort_on':    ['name','size'][args.order[0].column]});
+
+            // If another requests has come while we were waiting, simply drop this one.
+            if (i !== j) return null;
+
+            // Update cache info.
+            total          = result.total;
+            cacheStart     = args.start;
+            cache          = result.items;
+            // cacheFolder    = currentFolder;
+            cacheSortCol   = args.order[0].column;
+            cacheSortOrder = args.order[0].dir;
+
+            return cache.slice(args.start - cacheStart, args.length);
+        }
+    };
+
+    // The actual function passed to datatables.
+    // (needs a non-async wrapper cause datatables won't accept it otherwise)
+    let fn = (args, cb, settings) => (async () => {
+
+        let data = await get(args);
+        if (data === null)
+            return;
+
+        cb({'data':            data,
+            'recordsTotal':    total,
+            'recordsFiltered': total });
+    })();
+
+    // Allow manually clearing results (needed during soft-reload after uploading a file).
+    fn.dropCache = () => cache = [];
+    return fn;
+})();
+
+
+// Functions for rendering table cells, per column.
+const tableRenderer = {
+    name: (name, _, row) => {
+        return `<div class="list-group-item group" data-name="${name}" >${htmlEncode(name)}</div>`;
+    },
+    size: (size, _, row) => {
+        if (row.type === 'coll') {
+            return '';
+        } else {
+            let szs = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB'];
+            let szi = 0;
+            while (size >= 1024 && szi < szs.length-1) {
+                size /= 1024;
+                szi++;
+            }
+            return (Math.floor(size*10)/10+'') + '&nbsp;' + szs[szi];
+        }
+    }
+};
+
+
+function htmlEncode(value){
+    //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+    //then grab the encoded contents back out.  The div never exists on the page.
+    return $('<div/>').text(value).html().replace('"', '&quot;');
+}
+
+
+
+
