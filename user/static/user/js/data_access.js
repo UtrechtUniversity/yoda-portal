3fd6eca718b1b369b307df597a493f9fcf332edf
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-03-28 15:17:57 +0200

    Cleanup user javascript with Standard style.

--- user/static/user/js/data_access.js
+++ user/static/user/js/data_access.js
@@ -1,54 +1,54 @@
-"use strict";
+'use strict'
 
-$(document).ready(function() {
-    $("body").on("click", ".delete-token", function(e) {
-        let label = $(this).siblings('label').text();
-        Yoda.call('token_delete', {"label": label}, {"quiet": true}).then(
-            (data) => {
-                $(this).parent().remove();
-            },
-            (error) => {
-                 Yoda.set_message(
-                    'error',
-                    `An error occurred while deleting the data access password. If the issue persists, contact your administrator.`);
-            });
-    });
+$(document).ready(function () {
+  $('body').on('click', '.delete-token', function (e) {
+    const label = $(this).siblings('label').text()
+    Yoda.call('token_delete', { label }, { quiet: true }).then(
+      (data) => {
+        $(this).parent().remove()
+      },
+      (error) => {
+        Yoda.set_message(
+          'error',
+          'An error occurred while deleting the data access password. If the issue persists, contact your administrator.')
+      })
+  })
 
-    $("body").on("click", "#generateButton", function(e){
-        let label = $("#f-token-label").val();
+  $('body').on('click', '#generateButton', function (e) {
+    const label = $('#f-token-label').val()
 
-        let button = document.getElementById('generateButton');
-        button.setAttribute("hidden", true);
+    const button = document.getElementById('generateButton')
+    button.setAttribute('hidden', true)
 
-        Yoda.call("token_delete_expired",{}).then(response => {
-            return Yoda.call("token_generate", {"label": label}, {"quiet": true}).then(
-                (data) => {
-                    $('#f-token').val(data);
-                    let p = document.getElementById('passwordOk');
-                    p.removeAttribute("hidden");
-                },
-                (error) => {
-                    let error_id = "passwordGenerateError";
-                    if (error.status === "error_TokenExistsError") {
-                        error_id = "passwordLabelError";
-                    }
-                    let p = document.getElementById(error_id);
-                    p.removeAttribute("hidden");
-                    button.removeAttribute("hidden");
-                }
-            );
-        });
-    });
+    Yoda.call('token_delete_expired', {}).then(response => {
+      return Yoda.call('token_generate', { label }, { quiet: true }).then(
+        (data) => {
+          $('#f-token').val(data)
+          const p = document.getElementById('passwordOk')
+          p.removeAttribute('hidden')
+        },
+        (error) => {
+          let errorId = 'passwordGenerateError'
+          if (error.status === 'error_TokenExistsError') {
+            errorId = 'passwordLabelError'
+          }
+          const p = document.getElementById(errorId)
+          p.removeAttribute('hidden')
+          button.removeAttribute('hidden')
+        }
+      )
+    })
+  })
 
-    $('.btn-copy-to-clipboard').click(function(event){
-        $('#f-token').select();
-        document.execCommand("copy");
-        event.preventDefault();
-    });
+  $('.btn-copy-to-clipboard').click(function (event) {
+    $('#f-token').select()
+    document.execCommand('copy')
+    event.preventDefault()
+  })
 
-    var passwordModal = document.getElementById('dataAccessPassword');
-    passwordModal.addEventListener('hidden.bs.modal', function (event) {
-        $(this).find('form').trigger('reset');
-        window.location.reload();
-    });
-});
+  const passwordModal = document.getElementById('dataAccessPassword')
+  passwordModal.addEventListener('hidden.bs.modal', function (event) {
+    $(this).find('form').trigger('reset')
+    window.location.reload()
+  })
+})
