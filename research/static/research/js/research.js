3330b938ec35a73343af869ac8d3d2df4cad9560
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-04-19 11:51:25 +0200

    jQuery change event shorthand is deprecated.

--- research/static/research/js/research.js
+++ research/static/research/js/research.js
@@ -422,7 +422,7 @@ $(function () {
     }
   })
 
-  $('#file-formats-list').change(function () {
+  $('#file-formats-list').on('change', function () {
     const folder = $('a.action-check-for-unpreservable-files').attr('data-folder')
     const list = $('#file-formats-list option:selected').val()
     if (!(list in preservableFormatsLists)) { return }
