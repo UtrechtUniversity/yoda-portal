30a370c0c537057531b85542e55bf29621a83223
Roy van Elk <github@royvanelk.nl>
2023-06-27 10:30:13 +0200

    Add person identifier & vault upgrade

--- research/metadata-form/src/index.js
+++ research/metadata-form/src/index.js
@@ -7,6 +7,7 @@ import Select from 'react-select';
 import Geolocation from "./Geolocation";
 import Vocabulary from "./Vocabulary";
 import ROR from  "./ROR";
+import PersonIdentifier from "./PersonIdentifier";
 
 const path = $('#form').attr('data-path');
 
@@ -115,7 +116,8 @@ const widgets = {
 const fields = {
     geo: Geolocation,
     vocabulary: Vocabulary,
-    ror: ROR
+    ror: ROR,
+    person_identifier: PersonIdentifier
 };
 
 const CustomArrayFieldTemplate = (props) => {
