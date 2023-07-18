903c1e90f1d8f38435c645ebaaaa4520bebef84d
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-07-18 11:27:54 +0200

    Cleanup Geolocation in metadata form with standard.

--- vault/metadata-form/webpack.config.js
+++ vault/metadata-form/webpack.config.js
@@ -1,31 +1,31 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 
-var BUILD_DIR = path.resolve(__dirname, '../static/vault/js');
-var APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, '../static/vault/js')
+const APP_DIR = path.resolve(__dirname, 'src')
 
-var config = {
-    entry: APP_DIR + '/index.js',
-    output: {
-        path: BUILD_DIR,
-        filename: 'metadata-form.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?/,
-                include : APP_DIR,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/react'],
-                        plugins: ['@babel/plugin-proposal-object-rest-spread']
-                    }
-                }
-            }
-        ]
-    },
-    mode: "production"
-};
+const config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'metadata-form.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        include: APP_DIR,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'production'
+}
 
-module.exports = config;
+module.exports = config
