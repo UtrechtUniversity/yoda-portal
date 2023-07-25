21dd1346f8c1f7f56a3546cf1fed487129a585b7
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-07-25 08:20:12 +0200

    YDA-5148: implement iRODS session cleanups
    This reduces the impact of resource leaks bugs, such as memory leaks
    on portal sessions.
    
    (backport to Yoda 1.8.x)

--- deposit/deposit.py
+++ deposit/deposit.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-__copyright__ = 'Copyright (c) 2021-2022, Utrecht University'
+__copyright__ = 'Copyright (c) 2021-2023, Utrecht University'
 __license__ = 'GPLv3, see LICENSE'
 
 import io
 from typing import Iterator
 
-from flask import abort, Blueprint, g, redirect, render_template, request, Response, stream_with_context, url_for
+from flask import abort, Blueprint, g, redirect, render_template, request, Response, session, stream_with_context, url_for
 from irods.exception import CAT_NO_ACCESS_PERMISSION
 
 import api
+import connman
 
 deposit_bp = Blueprint('deposit_bp', __name__,
                        template_folder='templates',
@@ -58,17 +59,16 @@ def data() -> Response:
 def download() -> Response:
     path = '/' + g.irods.zone + '/home' + request.args.get('filepath')
     filename = path.rsplit('/', 1)[1]
-    session = g.irods
-
     READ_BUFFER_SIZE = 1024 * io.DEFAULT_BUFFER_SIZE
 
     def read_file_chunks(path: str) -> Iterator[bytes]:
-        obj = session.data_objects.get(path)
+        obj = g.irods.data_objects.get(path)
         try:
             with obj.open('r') as fd:
                 while True:
                     buf = fd.read(READ_BUFFER_SIZE)
                     if buf:
+                        connman.extend(session.sid)
                         yield buf
                     else:
                         break
@@ -77,7 +77,7 @@ def download() -> Response:
         except Exception:
             abort(500)
 
-    if session.data_objects.exists(path):
+    if g.irods.data_objects.exists(path):
         return Response(
             stream_with_context(read_file_chunks(path)),
             headers={
