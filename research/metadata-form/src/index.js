cce64befb1330d728145a907a3e462794de58ee7
Harm de Raaff <harm.apple@gmail.com>
2023-08-02 12:58:51 +0200

    initial fix in not presenting delete button first row

--- research/metadata-form/src/index.js
+++ research/metadata-form/src/index.js
@@ -195,7 +195,7 @@ const CustomArrayFieldTemplate = (props) => {
                                         </div>
                                     )}
 
-                                    {el.hasRemove && (
+                                    {el.hasRemove && el.index>0 && (
                                         <div className="m-0 p-0">
                                             <button className="btn btn-light btn-sm" type="button" tabindex="-1"
                                                     onClick={el.onDropIndexClick(el.index)}>
