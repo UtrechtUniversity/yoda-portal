5e59722b984d99d79840ba27b51b84ea81bcba5b
Sietse Snel <s.t.snel@uu.nl>
2023-10-25 11:37:16 +0200

    YDA-5506: fix subdomain handling OIDC redirect
    The old function did not handle wildcard subdomains (e.g. '*.cs.uu.nl')
    correctly. Also decompose function to facilitate adding a unit test.

--- util.py
+++ util.py
@@ -5,6 +5,7 @@ __license__   = 'GPLv3, see LICENSE'
 
 import sys
 import traceback
+from typing import List
 
 
 def log_error(message: str, print_exception: bool = False) -> None:
@@ -17,3 +18,25 @@ def log_error(message: str, print_exception: bool = False) -> None:
     print(message, file=sys.stderr)
     if print_exception:
         traceback.print_exc()
+
+
+def is_email_in_domains(email: str, domain_list: List[str]) -> bool:
+    """Determines if an email address is in a list of domains
+
+       :param email: email address of a user
+       :param domain_list: list of domains, which can also include wildcard domains that
+                           match a domain and any of its subdomains
+                           (e.g. "*.uu.nl" matches both user@uu.nl, user@subdomain.uu.nl)
+
+       :returns: boolean value that indicates whether this email address is in one of the
+                 domains
+    """
+    for domain in domain_list:
+        if domain.startswith("*."):
+            if email.endswith(domain[1:]) or email.endswith("@" + domain[2:]):
+                return True
+        else:
+            if email.endswith("@" + domain):
+                return True
+
+    return False
