38f897b88aea0c0a4625c67d038d8bf1b7de4ac5
claravox <claravox@users.noreply.github.com>
2024-01-10 09:41:59 +0100

    Metadata form: protect path variable

--- research/research.py
+++ research/research.py
@@ -14,6 +14,7 @@ from flask import (
 from irods.data_object import iRODSDataObject
 from irods.exception import CAT_NO_ACCESS_PERMISSION
 from irods.message import iRODSMessage
+from requests.utils import quote
 from werkzeug.utils import secure_filename
 
 import api
@@ -142,7 +143,7 @@ def upload_get() -> Response:
         else:
             raise Exception
     except Exception:
-        # Chunk does not exists and needs to be uploaded.
+        # Chunk does not exist and needs to be uploaded.
         response = make_response(jsonify({"message": "Chunk not found"}), 204)
         response.headers["Content-Type"] = "application/json"
         return response
@@ -220,8 +221,11 @@ def upload_post() -> Response:
 @research_bp.route('/metadata/form')
 def form() -> Response:
     path = request.args.get('path')
+    escape_path = quote(path)
+    if path != escape_path:
+        abort(404)
 
-    return render_template('research/metadata-form.html', path=path)
+    return render_template('research/metadata-form.html', path=escape_path)
 
 
 @research_bp.route('/browse/download_checksum_report')
