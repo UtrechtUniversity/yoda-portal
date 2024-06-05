cd0677477d8c4942933d5bdb15d6dd7d8943b841
FuHsinyu <x.fu1@uu.nl>
2024-06-05 14:44:26 +0200

    improve code styles

--- admin/admin.py
+++ admin/admin.py
@@ -3,19 +3,16 @@
 __copyright__ = 'Copyright (c) 2021, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
-# Blueprint for modular structure of codes
-# redirect for directing users to various url
-# render_template is based on Jinja2
-from flask import Blueprint, render_template, Response,
+from flask import Blueprint, render_template, Response
 
 admin_bp = Blueprint('admin_bp', __name__,
-                       template_folder='templates/admin',
-                       static_folder='static/admin',
-                       static_url_path='/assets')
+                     template_folder='templates/admin',
+                     static_folder='static/admin',
+                     static_url_path='/assets')
 
 
-@admin_bp.route('/') # Root url
-def index() -> Response: #Type Hint, response is a class in Flask
-    #TODO: ADD category response from api (talked to irods)
-    # E.g,: category_response = api.call('resource_category_stats', data={})
+@admin_bp.route('/')
+def index() -> Response:
+    # TODO: ADD category response for communicating to irods
+    # E.g.,: category_response = api.call('resource_category_stats', data={})
     return render_template('admin.html')
