d1e51fe10364f1c29d538d99bd839278b642fac6
FuHsinyu <x.fu1@uu.nl>
2024-06-12 12:41:08 +0200

    add user right check as API call

--- admin/admin.py
+++ admin/admin.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
-__copyright__ = 'Copyright (c) 2024, Utrecht University'
-__license__   = 'GPLv3, see LICENSE'
+__copyright__ = "Copyright (c) 2024, Utrecht University"
+__license__ = "GPLv3, see LICENSE"
 
-from flask import Blueprint, render_template, Response
+from flask import Blueprint, g, render_template, Response
 
-admin_bp = Blueprint('admin_bp', __name__,
-                     template_folder='templates/admin',
-                     static_folder='static/admin',
-                     static_url_path='/assets')
+import api
+
+admin_bp = Blueprint(
+    "admin_bp",
+    __name__,
+    template_folder="templates/admin",
+    static_folder="static/admin",
+    static_url_path="/assets",
+)
 
 
-@admin_bp.route('/')
+@admin_bp.route("/")
 def index() -> Response:
-    # TODO: ADD category response for communicating to irods
-    # E.g.,: category_response = api.call('resource_category_stats', data={})
-    return render_template('admin.html')
+    # Call api to check is user is admin
+    is_admin = api.call("admin_is_user_admin", {})
+    if (
+        is_admin
+    ):  # TODO redirect to the access-dinied html (available) for non-admin user
+        print("Test api_group_user_is_admin success, from Portal")
+
+    return render_template("admin.html")
