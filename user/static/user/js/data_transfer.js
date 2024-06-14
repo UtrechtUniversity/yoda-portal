d92d520a8e792acb5dd521c3cd554f8e9580176d
leonidastri <leonidas-tri@hotmail.com>
2024-06-14 12:07:37 +0200

    YDA-5737: fix buttons placement

--- user/static/user/js/data_transfer.js
+++ user/static/user/js/data_transfer.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
     hljs.highlightAll();
 
+    var snippets = document.getElementsByTagName('pre');
+    for (var i = 0; i < snippets.length; i++) {
+        code = snippets[i].getElementsByTagName('code')[0].innerText;
+
+        snippets[i].classList.add('hljs'); // append copy button to pre tag
+        snippets[i].innerHTML = '<button id="button' + (i+1) + '" class="hljs-copy btn btn-secondary btn-copy-to-clipboard" style="float: right;"><i class="fa fa-copy"></i> Copy</button>' + snippets[i].innerHTML;
+    }
+
     $('.btn-copy-to-clipboard').on('click', function(event) {
         if (this.id == 'button1')
             var codeBlockId = "code-block1"
         else
             var codeBlockId = "code-block2"
 
+        console.log(this.id + ' ' + codeBlockId)
         const codeContent = document.getElementById(codeBlockId).textContent;
 
         var textArea = document.createElement('textarea');
