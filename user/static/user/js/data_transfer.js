a0d5c3f6212a8cf1b15c821161ec34720db661e0
leonidastri <leonidas-tri@hotmail.com>
2024-06-14 14:11:36 +0200

    YDA-5737: add margin for copy button

--- user/static/user/js/data_transfer.js
+++ user/static/user/js/data_transfer.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         code = snippets[i].getElementsByTagName('code')[0].innerText;
 
         snippets[i].classList.add('hljs'); // append copy button to pre tag
-        snippets[i].innerHTML = '<button id="button' + (i+1) + '" class="hljs-copy btn btn-secondary btn-copy-to-clipboard" style="float: right;"><i class="fa fa-copy"></i> Copy</button>' + snippets[i].innerHTML;
+        snippets[i].innerHTML = '<button id="button' + (i+1) + '" class="hljs-copy btn btn-secondary btn-copy-to-clipboard mt-2 me-2" style="float: right;"><i class="fa fa-copy"></i> Copy</button>' + snippets[i].innerHTML;
     }
 
     $('.btn-copy-to-clipboard').on('click', function(event) {
