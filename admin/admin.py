b89733caa5ae3d664bbb74c612c46f2d6164444c
Lazlo Westerhof <l.r.westerhof@uu.nl>
2024-07-03 08:28:39 +0200

    Small cleanup.

--- admin/admin.py
+++ admin/admin.py
@@ -1,19 +1,16 @@
 #!/usr/user/env python3
 
 __copyright__ = "Copyright (c) 2024, Utrecht University"
-__license__ = "GPLv3, see LICENSE"
+__license__   = "GPLv3, see LICENSE"
 
 from flask import abort, Blueprint, render_template, Response
 
 import api
 
-admin_bp = Blueprint(
-    "admin_bp",
-    __name__,
-    template_folder="templates/admin",
-    static_folder="static/admin",
-    static_url_path="/assets",
-)
+admin_bp = Blueprint("admin_bp", __name__,
+                     template_folder="templates/admin",
+                     static_folder="static/admin",
+                     static_url_path="/assets")
 
 
 @admin_bp.route("/")
