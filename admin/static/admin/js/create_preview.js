d6afcd7ac5857c9f7822f9b0a76c3093db5b7444
“FuHsinyu” <x.fu1@uu.nl>
2024-08-06 12:40:31 +0200

    Add DOMpurify to santinize html content

--- admin/static/admin/js/create_preview.js
+++ admin/static/admin/js/create_preview.js
@@ -1,11 +1,12 @@
 // Function to preview publication terms in a specific modal
 function createPreview() {
-    // Get the content of the textarea
+    // Get the content of the textarea and sanitize it
     var termsText = document.getElementById('publicationTerms').value;
+    const sanitizedContent = DOMPurify.sanitize(termsText);
 
     // Set the content in the modal body for preview
     const modalBody = document.querySelector('#confirmAgreementConditions .modal-body');
-    modalBody.innerHTML = termsText;
+    modalBody.innerHTML = sanitizedContent;
 
     // Show the modal
     var myModal = new bootstrap.Modal(document.getElementById('confirmAgreementConditions'));
