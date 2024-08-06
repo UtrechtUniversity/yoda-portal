7a4723327dc6657ffd0a2580f5251d1de16b4a45
Lazlo Westerhof <l.r.westerhof@uu.nl>
2024-08-06 14:10:55 +0200

    Add missing document ready

--- admin/static/admin/js/create_preview.js
+++ admin/static/admin/js/create_preview.js
@@ -1,17 +1,19 @@
 /* global bootstrap, DOMPurify */
 'use strict'
 
-// Preview publication terms in a modal.
-$('.btn-create-preview').on('click', function () {
-  // Get the content of the textarea and sanitize it.
-  const termsText = document.getElementById('publicationTerms').value
-  const sanitizedContent = DOMPurify.sanitize(termsText)
+$(document).ready(function () {
+  // Preview publication terms in a modal.
+  $('.btn-create-preview').on('click', function () {
+    // Get the content of the textarea and sanitize it.
+    const termsText = document.getElementById('publicationTerms').value
+    const sanitizedContent = DOMPurify.sanitize(termsText)
 
-  // Set the content in the modal body for preview.
-  const modalBody = document.querySelector('#confirmAgreementConditions .modal-body')
-  modalBody.innerHTML = sanitizedContent
+    // Set the content in the modal body for preview.
+    const modalBody = document.querySelector('#confirmAgreementConditions .modal-body')
+    modalBody.innerHTML = sanitizedContent
 
-  // Show the modal.
-  const myModal = new bootstrap.Modal(document.getElementById('confirmAgreementConditions'))
-  myModal.show()
+    // Show the modal.
+    const myModal = new bootstrap.Modal(document.getElementById('confirmAgreementConditions'))
+    myModal.show()
+  })
 })
