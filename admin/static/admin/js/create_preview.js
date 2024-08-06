7c954c264646d05eb5c1bb34f2f3c1746afc2495
Lazlo Westerhof <l.r.westerhof@uu.nl>
2024-08-06 14:03:32 +0200

    Refactor to remove eslint ignore

--- admin/static/admin/js/create_preview.js
+++ admin/static/admin/js/create_preview.js
@@ -1,19 +1,17 @@
 /* global bootstrap, DOMPurify */
 'use strict'
 
-/* eslint-disable no-unused-vars */
-// Function to preview publication terms in a specific modal
-function createPreview () {
-  // Get the content of the textarea and sanitize it
+// Preview publication terms in a modal.
+$('.btn-create-preview').on('click', function () {
+  // Get the content of the textarea and sanitize it.
   const termsText = document.getElementById('publicationTerms').value
   const sanitizedContent = DOMPurify.sanitize(termsText)
 
-  // Set the content in the modal body for preview
+  // Set the content in the modal body for preview.
   const modalBody = document.querySelector('#confirmAgreementConditions .modal-body')
   modalBody.innerHTML = sanitizedContent
 
-  // Show the modal
+  // Show the modal.
   const myModal = new bootstrap.Modal(document.getElementById('confirmAgreementConditions'))
   myModal.show()
-}
-/* eslint-enable no-unused-vars */
+})
