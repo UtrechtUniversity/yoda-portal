5de6065104d2978595c82daf623ae67945de8510
FuHsinyu <x.fu1@uu.nl>
2024-08-12 15:34:49 +0200

    add prefix admin for button ids

--- admin/static/admin/js/create_preview.js
+++ admin/static/admin/js/create_preview.js
@@ -3,9 +3,9 @@
 
 $(document).ready(function () {
   // Preview publication terms in a modal.
-  $('#create-preview').on('click', function () {
+  $('#admin-create-preview').on('click', function () {
     // Get the content of the textarea and sanitize it.
-    const termsText = document.getElementById('publicationTerms').value
+    const termsText = document.getElementById('admin-publication-terms').value
     const sanitizedContent = DOMPurify.sanitize(termsText)
 
     // Set the content in the modal body for preview.
