441eafac831f30dcc9034b6c85a04b72ca28e76c
Dylan Hsin <x.fu1@uu.nl>
2024-08-16 12:18:36 +0200

    YDA-5877: improve admin page UI consistency

--- admin/static/admin/js/create_preview.js
+++ admin/static/admin/js/create_preview.js
@@ -3,9 +3,9 @@
 
 $(document).ready(function () {
   // Preview publication terms in a modal.
-  $('#create-preview').on('click', function () {
+  $('#admin-create-preview').on('click', function () {
     // Get the content of the textarea and sanitize it.
-    const termsText = document.getElementById('publicationTerms').value
+    const termsText = document.getElementById('admin-publication-terms').value
     const sanitizedContent = DOMPurify.sanitize(termsText)
 
     // Set the content in the modal body for preview.
@@ -15,5 +15,10 @@ $(document).ready(function () {
     // Show the modal.
     const myModal = new bootstrap.Modal(document.getElementById('confirmAgreementConditions'))
     myModal.show()
+  })
+
+  // Click the Confirm button to dismiss
+  $('#confirmAgreementConditions').on('click', '.action-confirm-submit-for-publication', function () {
+    $('#confirmAgreementConditions').modal('hide')
   })
 })
