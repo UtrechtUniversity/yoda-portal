31109cef8507b45bc1a21f69324e62916476350d
claravox <claravox@users.noreply.github.com>
2024-11-19 11:05:19 +0100

    YDA-5059: keyword selector almost working

--- research/metadata-form/src/KeywordSelector.js
+++ research/metadata-form/src/KeywordSelector.js
@@ -1,178 +1,150 @@
 import React from "react";
-import Modal from 'react-modal';
 import axios from "axios";
+import { TreeSelect, ConfigProvider, theme } from 'antd';
 
-var self = null;
-let json = {};
-
-const customModalStyles = {
-    content : {
-        top                   : '50%',
-        left                  : '50%',
-        right                 : 'auto',
-        bottom                : 'auto',
-        marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)',
-        width                 : '70%',
-        height                : '625px',
-    }
-};
 
 class KeywordSelector extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...props.formData
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+        ...props.formData,
+        value: [],
+        treeData: null
+    };
 
-        const url = 'https://raw.githubusercontent.com/UtrechtUniversity/yoda-ruleset/epos-msl/vocabularies/epos-msl-keywords.json';
-        const loadData = async () => {
-            await axios.get(url).then((res) => {
-                this.json = res.data;
-            });
-        };
-        loadData();
+    // Check what theme is set
+    // TODO should this be a prop passed down from index.js?
+    this.colorMode = document.documentElement.getAttribute('data-bs-theme');
+    this.colorPrimary = getComputedStyle(document.documentElement).getPropertyValue('--bs-primary');
+    this.lightModeSelectColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-black');
+    this.lightModeSelectBgColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-secondary-bg');
+    this.darkModeSelectColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-body-bg');
+    // TODO This color is slightly blue idk why
+    // this.darkModeSelectBgColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-gray-dark');
+    this.darkModeSelectBgColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-tertiary-color');
+  }
 
-        this.openModal = this.openModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
-        this.afterOpenModal = this.afterOpenModal.bind(this);
-        this.addKeyword = this.addKeyword.bind(this);
+  async loadData() {
+    const url = 'https://raw.githubusercontent.com/UtrechtUniversity/msl_vocabularies/refs/heads/main/vocabularies/combined/editor/1.3/editor_1-3.json';
+    const res = await axios(url);
+    return await res.data;
+  }
+
+  componentDidMount () {
+    const newVal = this.props.formData ? this.props.formData.map((keyObj) => ({"label": keyObj.Subject, "value": [keyObj.Subject, keyObj.valueURI].join(":")})) : []
+    if (!this.state.treeData) {
+      this.loadData().then(data => this.setState({
+        treeData: this.createTreeSelectFromJson(data),
+        value: newVal
+      }))
+    }
+  }
+
+  createTreeSelectFromJson = (jsonA) => {
+    const newTreeJson = []
+    for (const index in jsonA) {
+      const tree = jsonA[index]
+      const newTree = {}
+      newTree["value"] = tree["text"] + ":" + tree["extra"]["uri"]
+      newTree["title"] = tree["text"]
+
+      if ('children' in tree) {
+        newTree["children"] = this.createTreeSelectFromJson(tree['children'])
+      }
+      newTreeJson.push(newTree)
+    }
+    return newTreeJson
+  }
+
+  putCheckOnParents = (treeJson, item, selected) => {
+    /* Recursively go down the tree to find the selected (one) item
+       After hitting the base case, traverse back up to the root of the tree,
+       selecting all parents and ancestors
+    */
+    for (const i in treeJson) {
+      const tree = treeJson[i]
+
+      if (tree.value === item.value) {
+        return true
+      }
+
+      if ('children' in tree) {
+        if (this.putCheckOnParents(tree["children"], item, selected)) {
+          selected.push({"label": tree["title"], "value": tree["value"]})
+          // if we found the item, don't need to visit other branches
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+
+  onChange = (newValue) => {
+    if (newValue.length > this.state.value.length) {
+      // Find the new node
+      const newNodes = newValue.filter(({value: id2}) => !this.state.value.some(({value: id1}) => id2 === id1))
+      this.putCheckOnParents(this.state.treeData, newNodes[0], newValue)
     }
 
-    openModal(e) {
-        e.preventDefault();
+    this.setState({
+        "value": newValue
+    },
+    () => this.props.onChange(this.state));
+  }
 
-        this.showModal = true;
-        this.setState(this.state);
-    }
-
-    closeModal(e) {
-        e.preventDefault();
-
-        this.showModal = false;
-        this.setState(this.state);
-    }
-
-    afterOpenModal(e) {
-        self = this;
-
-        $.jstree.defaults.core.themes.responsive = true;
-
-        $('#tree').jstree({
-            plugins: ["checkbox", "wholerow", "search"],
-            "types": {
-                "file": {
-                    "icon": "jstree-file"
-                }
-            },
-            'core': {
-                'data': this.json
-            },
-            checkbox: {
-                three_state : false, // to avoid that fact that checking a node also check others
-                whole_node : false,  // to avoid checking the box just clicking the node
-                tie_selection : false // for checking without selecting and selecting without checking
-            },
-            "search": {
-                "case_sensitive": false,
-                "show_only_matches": true
-            }
-        })
-        .on("check_node.jstree uncheck_node.jstree", function(e, data) {
-            data.node.parents.forEach((element) => {
-                $('#tree').jstree('check_node', element);
-            });
-
-            if(e.type == "check_node") {
-                self.addItem(data.node);
-            } else if (e.type == "uncheck_node") {
-                self.removeItem(data.node);
-            }
-        });
-
-        $(document).ready(function () {
-            $("#search-input").keyup(function () {
-                var searchString = $(this).val();
-                $('#tree').jstree('search', searchString);
-            });
-        });
-    }
-
-    addKeyword(keyword, valueUri) {
-        this.setState({"keywords": [
-                {"keyword": keyword, "valueURI": valueUri}
-        ]}, () => this.props.onChange(this.state));
-    }
-
-    addItem(node) {
-        $("#modal-list-group").append(
-            '<li class="list-group-item" id="modal-list-group-item-' + node.id +'">' + node.text + '<a href="#" id="modal-list-group-item-delete-' + node.id + '" title="remove keyword"><i class="bi bi-x text-danger"></i></a></li>'
-        );
-
-        $('#modal-list-group-item-delete-' + node.id).bind("click", function(){
-            $('#modal-list-group-item-' + node.id).remove();
-            $("#tree").jstree("uncheck_node", node.id);
-        });
-
-        this.addKeyword(node.text, node.original.extra.uri);
-    }
-
-    removeItem(node) {
-        $('#modal-list-group-item-' + node.id).remove();
-    }
-
-    render() {
-        return (
-            <div className='col-12 field-wrapper'>
-                <div className='form-group mb-0'>
-                    <div className='mb-0 form-group keyword-selector'>
-                        <label className='w-100'>{this.props.schema.title}</label>
-                        <button className='btn btn-outline-secondary' type='button' tabIndex='-1'
-                                onClick={(e) => {
-                                    this.openModal(e);
-                                }}>Select keywords
-                        </button>
-                        <Modal
-                            isOpen={this.showModal}
-                            onAfterOpen={this.afterOpenModal}
-                            onRequestClose={this.closeModal}
-                            style={customModalStyles}
-                            ariaHideApp={false}
-                        >
-                            <div class='container-fluid'>
-                                <div class='row'>
-                                    <div class='col-md-12'>
-                                        <div class='input-group'>
-                                            <input id='search-input' class='form-control' placeholder='Search for keyword...' />
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr />
-                                <div class='row'>
-                                    <div class='col-md-6'>
-                                        <div id="tree"></div>
-                                    </div>
-                                    <div class='col-md-6'>
-                                        <ul class='list-group' id='modal-list-group'>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </Modal>
+  render() {
+    return (
+        <div className='col-12 field-wrapper'>
+            <div className='form-group mb-0'>
+                <div className='mb-0 form-group keyword-selector'>
+                    <label className='w-100'>{this.props.schema.title}</label>
+                    <ConfigProvider
+                      theme={{
+                        algorithm: this.colorMode === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm,
+                        token: {
+                          colorPrimary: this.colorPrimary,
+                        },
+                        components: {
+                          TreeSelect: {
+                            // nodeSelectedColor: this.colorMode === 'dark' ? this.darkModeSelectColor : this.lightModeSelectColor,
+                            nodeSelectedBg: this.colorMode === 'dark' ? this.darkModeSelectBgColor : this.lightModeSelectBgColor,
+                            // nodeHoverColor: this.colorMode === 'dark' ? this.darkModeSelectColor : this.lightModeSelectColor,
+                            nodeHoverBg: this.colorMode === 'dark' ? this.darkModeSelectBgColor : this.lightModeSelectBgColor,
+                          },
+                        },
+                      }}
+                    >
+                      <TreeSelect
+                          labelInValue
+                          showSearch
+                          style={{
+                            width: '100%',
+                          }}
+                          // dropdownStyle={{
+                          //   maxHeight: 400,
+                          //   overflow: 'auto',
+                          // }}
+                          value={this.state.value}
+                          placeholder="Select keyword"
+                          placement="bottomLeft"
+                          allowClear
+                          multiple
+                          disabled={this.props.readonly}
+                          onChange={this.onChange}
+                          treeData={this.state.treeData}
+                      />
+                    </ConfigProvider>
+                    <small className="text-muted form-text">
                         <small className="text-muted form-text">
-                            <small className="text-muted form-text">
-                                {this.props.uiSchema["ui:description"]}
-                            </small>
+                            {this.props.uiSchema["ui:description"]}
                         </small>
-                        {this.props.formData.Keywords &&
-                         this.props.formData.Keywords.map(el => (
-                        <div>test</div>
-                        ))}
-                    </div>
+                    </small>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+  }
 }
 
 export default KeywordSelector;
