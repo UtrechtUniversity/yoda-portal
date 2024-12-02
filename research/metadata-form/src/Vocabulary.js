f5b6523235235c244f0f03b0e24b0de3262a67d6
claravox <claravox@users.noreply.github.com>
2024-12-02 14:22:21 +0100

    YDA-5059: add custom keyword support

--- research/metadata-form/src/Vocabulary.js
+++ research/metadata-form/src/Vocabulary.js
@@ -50,76 +50,8 @@ class Vocabulary extends React.Component {
         })
     }
 
-    const darkThemeColors = {
-        /* For theme color guidance: https://github.com/JedWatson/react-select/issues/3692#issuecomment-523425096 */
-        /*
-         * control/backgroundColor
-         * menu/backgroundColor
-         * option/color(selected)
-         */
-        neutral0: '#212529',
-
-        /*
-         * control/backgroundColor(disabled)
-         */
-        neutral5: '#212529',
-
-        /*
-         * control/borderColor(disabled)
-         * multiValue/backgroundColor
-         * indicators(separator)/backgroundColor(disabled)
-         */
-        neutral10: '#343a40',
-
-        /*
-         * control/borderColor
-         * option/color(disabled)
-         * indicators/color
-         * indicators(separator)/backgroundColor
-         * indicators(loading)/color
-         */
-        neutral20: '#343a40',
-
-        /*
-         * control/borderColor(focused)
-         * control/borderColor:hover
-         */
-        neutral30: '#343a40',
-
-        /*
-         * input/color
-         * multiValue(label)/color
-         * singleValue/color
-         * indicators/color(focused)
-         * indicators/color:hover(focused)
-         */
-        neutral80: 'var(--neutral-10)',
-        neutral90: 'var(--neutral-10)',
-
-         /*
-          * One of the few bootstrap variables we can use with themeing react-select!
-          * control/boxShadow(focused)
-          * control/borderColor(focused)
-          * control/borderColor:hover(focused)
-          * option/backgroundColor(selected)
-          * option/backgroundColor:active(selected)
-          */
-        primary: 'var(--bs-primary)',
-
-        /*
-         * option/backgroundColor(focused)
-         */
-        primary25: '#2b3035',
-
-        /*
-         * option/backgroundColor:active
-         */
-        primary50: '#2b3035',
-        primary75: '#2b3035',
-    };
-
     // Check what theme is set
-    const colorMode = document.documentElement.getAttribute('data-bs-theme');
+    const colorMode = this.props.formContext.colorMode
 
     const required = this.props.required
     const error = 'should be equal to one of the allowed values'
@@ -152,8 +84,8 @@ class Vocabulary extends React.Component {
           onChange={(event) => this.props.onChange(event.value)}
           styles={customStyles}
           theme={(theme) => ({
-              ...theme,
-              colors: (colorMode === 'dark') ? {...theme.colors, ...darkThemeColors} : {...theme.colors},
+            ...theme,
+            colors: (colorMode === 'dark') ? { ...theme.colors, ...this.props.formContext.darkThemeColors } : { ...theme.colors }
           })}
         />
         {help && (
