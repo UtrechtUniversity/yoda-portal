bb1d26dae4c94874e2a82a0625e5f087079a4a8b
leonidastri <l.triantafyllou@uu.nl>
2025-01-22 11:01:47 +0100

    feat: parse csv functionality

--- group_manager/static/group_manager/js/group_manager.js
+++ group_manager/static/group_manager/js/group_manager.js
@@ -198,7 +198,7 @@ function readCsvFile (e) {
     let contents = e.target.result
 
     // remove unwanted characters
-    contents = contents.replaceAll('"', '').replaceAll("'", '').replaceAll(' ', '').replaceAll('\r', '')
+    contents = contents.replaceAll("'", '').replaceAll(' ', '').replaceAll('\r', '')
 
     // remove extra newline(s) at end of file
     while (contents[contents.length - 1] === '\n') {
@@ -214,8 +214,41 @@ function readCsvFile (e) {
     const csvRows = contents.slice(contents.indexOf('\n') + 1).split('\n')
     const csvRowsCorrected = []
 
-    // parse the csv file data to be able to present in a table
-    const result = csvToArray(contents)
+    // Parse the CSV data using PapaParse
+    let result
+    Papa.parse(contents, {
+      header: true, // Automatically treat the first row as headers
+      dynamicTyping: true, // Automatically detect data types (e.g., numbers, strings)
+      skipEmptyLines: true, // Ignore empty lines in the CSV
+      complete: function (parsedData) {
+        const headers = parsedData.meta.fields // Extract headers from the parsed result
+        const rows = parsedData.data // Extract rows as an array of objects
+
+        // Normalize headers (remove legacy suffixes like manager:, member:, viewer:)
+        headers.forEach(function (header, index) {
+          if (/^(manager|member|viewer):/.test(header)) {
+            headers[index] = header.substring(0, header.indexOf(':'))
+          }
+        })
+
+        // Map rows to align with normalized headers
+        result = rows.map(function (row) {
+          const normalizedRow = {}
+          headers.forEach(function (header) {
+            if (row[header] != null && row[header] !== '') {
+              if (!(header in normalizedRow)) {
+                normalizedRow[header] = []
+              }
+              normalizedRow[header].push(row[header]) // Add each value to the array
+            }
+          })
+          return normalizedRow
+        })
+
+        // Log or further process the result
+        console.log(result)
+      },
+    })
 
     // For compressing all columns to keys: category, subcategory, groupname, schema, expiration, and usercount
     const presentationColumns = ['groupname', 'category', 'subcategory', 'schema_id', 'expiration_date', 'users']
@@ -306,7 +339,8 @@ function readCsvFile (e) {
     table += '<td></td></tr></thead><tbody>'
 
     newResult.forEach(function myFunction (groupDef, i) {
-      table += '<tr id="' + groupDef.groupname + '" class="import-groupname" groupname="' + groupDef.groupname + '" importRowData="' + csvHeader + '\n' + csvRowsCorrected[i] + '">'
+      let safeRowData = csvRowsCorrected[i].replace(/"/g, '&quot;')
+      table += '<tr id="' + groupDef.groupname + '" class="import-groupname" groupname="' + groupDef.groupname + '" importRowData="' + csvHeader + '\n' + safeRowData + '">'
       table += '<td id="processed-indicator-' + groupDef.groupname + '"></td>'
       presentationColumns.forEach(function myFunction (column) {
         table += '<td>' + groupDef[column] + '</td>'
@@ -327,40 +361,6 @@ function readCsvFile (e) {
   reader.readAsText(file)
 }
 
-function csvToArray (str, delimiter = ',') {
-  const headers = str.slice(0, str.indexOf('\n')).split(delimiter)
-  const rows = str.slice(str.indexOf('\n') + 1).split('\n')
-
-  // If we have headers with legacy suffixes, normalize them by removing each suffix
-  // e.g. "manager:manager1" becomes "manager".
-  headers.forEach(function (element, number, headers) {
-    const headerValue = headers[number]
-    if (/^(manager|member|viewer):/.test(headerValue)) {
-      headers[number] = headerValue.substring(0, headerValue.indexOf(':'))
-    }
-  })
-
-  const arr = rows.map(function (row) {
-    const values = row.split(delimiter)
-    const el = headers.reduce(function (object, header, index) {
-      if (values[index] != null && values[index] !== '') {
-        if (!(header in object)) {
-          object[header] = []
-        }
-
-        object[header].push(values[index])
-      }
-      return object
-    }, {})
-    return el
-  })
-
-  if (arr.length > 0 && jQuery.isEmptyObject(arr[arr.length - 1])) {
-    return arr.slice(0, arr.length - 1)
-  }
-
-  return arr
-}
 
 async function processImportedRow (row) {
   // Row specific processing of the imported csv
