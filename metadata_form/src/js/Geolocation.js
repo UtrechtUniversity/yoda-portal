e396bb31389ab13db240c1a2d7216851ad5597ed
claudisa-uu <c.laudisa@uu.nl>
2025-02-26 17:16:28 +0100

    Fix dark mode for geolocation selector

--- metadata_form/src/js/Geolocation.js
+++ metadata_form/src/js/Geolocation.js
@@ -7,19 +7,6 @@ import { EditControl } from 'react-leaflet-draw'
 let globalGeoBoxCounter = 0 // Additions for being able to manually add geoBoxes
 let globalThis = null
 
-const customModalStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    width: '70%',
-    height: '625px'
-  }
-}
-
 class Geolocation extends React.Component {
   constructor (props) {
     super(props)
@@ -39,6 +26,11 @@ class Geolocation extends React.Component {
     this.geoBoxID = globalGeoBoxCounter
     this.showModal = false
     globalGeoBoxCounter++
+
+    this.coordsStyle = {
+      backgroundColor: this.props.formContext.colorMode === 'dark' ? '#212529' : '#fff',
+      border: this.props.formContext.colorMode === 'dark' ? '1px solid #495057' : '1px solid #ced4da',
+    }
   }
 
   openModal (e) {
@@ -233,7 +225,24 @@ class Geolocation extends React.Component {
           isOpen={this.showModal}
           onAfterOpen={this.afterOpenModal}
           onRequestClose={this.closeModal}
-          style={customModalStyles}
+          style={{
+            overlay: {
+              backgroundColor: '#212529BF',
+              zIndex: 1064
+            },
+            content: {
+              top: '50%',
+              left: '50%',
+              right: 'auto',
+              bottom: 'auto',
+              marginRight: '-50%',
+              transform: 'translate(-50%, -50%)',
+              width: '70%',
+              height: '625px',
+              backgroundColor: this.props.formContext.colorMode === 'dark' ? '#212529' : '#fffaf8',
+              border: this.props.formContext.colorMode === 'dark' ? '1px solid #495057' : '1px solid #ced4da',
+            },
+          }}
           ariaHideApp={false}
         >
           <MapContainer ref='map' center={[48.760, 13.275]} zoom={4} animate={false}>
@@ -265,10 +274,10 @@ class Geolocation extends React.Component {
 
           <div className='row'>
             <div className='col-sm-12 mt-1'>
-              <label>North:</label> <input type='text' className='geoInputCoords geoLat0 me-1' boxid={this.geoBoxID} disabled={this.props.readonly} />
-              <label>West:</label> <input type='text' className='geoInputCoords geoLng0 me-1' boxid={this.geoBoxID} disabled={this.props.readonly} />
-              <label>South:</label> <input type='text' className='geoInputCoords geoLat1' boxid={this.geoBoxID} disabled={this.props.readonly} />
-              <label>East:</label> <input type='text' className='geoInputCoords geoLng1 me-1' boxid={this.geoBoxID} disabled={this.props.readonly} />
+              <label>North:</label> <input type='text' className='geoInputCoords geoLat0 me-1' style={this.coordsStyle} boxid={this.geoBoxID} disabled={this.props.readonly} />
+              <label>West:</label> <input type='text' className='geoInputCoords geoLng0 me-1' style={this.coordsStyle} boxid={this.geoBoxID} disabled={this.props.readonly} />
+              <label>South:</label> <input type='text' className='geoInputCoords geoLat1 me-1' style={this.coordsStyle} boxid={this.geoBoxID} disabled={this.props.readonly} />
+              <label>East:</label> <input type='text' className='geoInputCoords geoLng1 me-1' style={this.coordsStyle} boxid={this.geoBoxID} disabled={this.props.readonly} />
               <button className='btn btn-outline-secondary float-end' onClick={(e) => { this.closeModal(e) }}>Close</button>
             </div>
           </div>
