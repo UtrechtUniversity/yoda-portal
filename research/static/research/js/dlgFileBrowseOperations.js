1a01515c638803e793c793ecabde576a0d5b9f10
Lazlo Westerhof <l.r.westerhof@uu.nl>
2025-07-15 15:03:53 +0200

    YDA-6078: don't add empty collections as files to download zip

--- research/static/research/js/dlgFileBrowseOperations.js
+++ research/static/research/js/dlgFileBrowseOperations.js
@@ -106,7 +106,12 @@ $(document).ready(function () {
         for (const item of data) {
           const filepath = `${path}/${item.name}`
           const url = `/research/browse/download?filepath=${encodeURIComponent(filepath)}`
-          downloadEntries.push({ url, name: name + '/' + item.name })
+
+          if (!item.name.endsWith('/')) {
+            downloadEntries.push({ url, name: name + '/' + item.name })
+          } else {
+            downloadEntries.push({ name: name + '/' + item.name })
+          }
         }
       } else {
         // Single file: add directly
