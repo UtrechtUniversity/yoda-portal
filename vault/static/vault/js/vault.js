317e1fe7891f2024ae6326f239b1528b71294408
claudisa-uu <c.laudisa@uu.nl>
2025-10-23 11:21:06 +0200

    Adjust checks and add comments

--- vault/static/vault/js/vault.js
+++ vault/static/vault/js/vault.js
@@ -775,9 +775,11 @@ function topInformation (dir, rebuildFileBrowser = false) {
       $('.btn-group button.metadata-form').hide()
 
       if (pathParts.length < 2) {
+        // Hide top information and buttons when browsing at vault space level
         $('.top-information').hide()
         $('.top-info-buttons').hide()
       } else {
+        // Show top information and buttons when browsing at group collection level and lower
         $('.top-information').show()
         $('.top-info-buttons').show()
       }
@@ -966,22 +968,22 @@ function topInformation (dir, rebuildFileBrowser = false) {
       // Reset action dropdown.
       $('.btn-group button.folder-status').prop('disabled', false).next().prop('disabled', false)
 
-      // Folder buttons
-      if (pathParts.length > 2) {
-        $('.top-information h2').html(`${statusBadge}${archiveBadge}${systemMetadataIcon}${actionLogIcon}`)
-      } else {
-        $('.top-information h2').hide()
-      }
-
       // Show top information and buttons.
       if (typeof vaultStatus !== 'undefined') {
+        // Arrange folder buttons of data package
+        $('.top-information h2').html(`${statusBadge}${archiveBadge}${systemMetadataIcon}${actionLogIcon}`)
+
         $('.top-information').show()
         $('.top-info-buttons').show()
 
         // Trigger tooltips.
         const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]:not(.download-report-text):not(.download-report-csv)')
         const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)) // eslint-disable-line no-unused-vars
+      } else {
+        // Clear folder buttons when not browsing a data package
+        $('.top-information h2').html('')
       }
+
       if (rebuildFileBrowser) {
         buildFileBrowser(dir)
       }
