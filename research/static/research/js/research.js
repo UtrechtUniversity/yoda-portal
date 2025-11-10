03dec2925e4f383dce719e2d8a3c86713adc4e39
FuHsinyu <x.fu1@uu.nl>
2025-11-10 14:31:08 +0100

    YDA-6644: add folder download button to context menu

--- research/static/research/js/research.js
+++ research/static/research/js/research.js
@@ -1104,6 +1104,7 @@ const tableRenderer = {
       if (currentFolder.length === 0) {
         return ''
       }
+      actions.append(`<a href="#" class="dropdown-item folder-download" data-path="${Yoda.htmlEncode(currentFolder + '/' + row.name)}" title="Download this folder">Download</a>`)
       actions.append(`<a href="#" class="dropdown-item folder-rename" data-collection="${Yoda.htmlEncode(currentFolder)}" data-name="${Yoda.htmlEncode(row.name)}" title="Rename this folder" >Rename</a>`)
       actions.append(`<a href="#" class="dropdown-item folder-copy" data-collection="${Yoda.htmlEncode(currentFolder)}" data-name="${Yoda.htmlEncode(row.name)}" title="Copy this folder">Copy</a>`)
       actions.append(`<a href="#" class="dropdown-item folder-move" data-collection="${Yoda.htmlEncode(currentFolder)}" data-name="${Yoda.htmlEncode(row.name)}" title="Move this folder">Move</a>`)
@@ -1154,7 +1155,7 @@ function startBrowsing () {
     drawCallback: function (settings) {
       const actions = {
         multi: ['a.multiple-copy', 'a.multiple-move', 'a.multiple-delete'],
-        folder: ['a.folder-delete', 'a.folder-rename', 'a.folder-copy', 'a.folder-move'],
+        folder: ['a.folder-download', 'a.folder-delete', 'a.folder-rename', 'a.folder-copy', 'a.folder-move'],
         file: ['a.file-delete', 'a.file-rename', 'a.file-copy', 'a.file-move']
       }
 
